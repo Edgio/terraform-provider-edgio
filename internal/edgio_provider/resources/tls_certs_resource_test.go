a13a5d6c75309197035c0b07a46c26f432336695
aperkucin <negyxo@gmail.com>
2024-11-14 15:33:57 +0100

    Fixed an issue with TlsCerts status "failed"

--- internal/edgio_provider/resources/tls_certs_resource_test.go
+++ internal/edgio_provider/resources/tls_certs_resource_test.go
@@ -1,6 +1,7 @@
 package resources_test
 
 import (
+	"regexp"
 	"testing"
 	"time"
 
@@ -243,6 +244,56 @@ func TestTLSCertsResource_UploadUpdate(t *testing.T) {
 					resource.TestCheckResourceAttr("edgio_tls_cert.uploaded", "common_name", "example.org"),
 					resource.TestCheckResourceAttr("edgio_tls_cert.uploaded", "primary_cert", "Changed"),
 				),
+			},
+		},
+	})
+
+	mockClient.AssertExpectations(t)
+}
+
+func TestTLSCertsResource_StateFailed(t *testing.T) {
+	mockClient := new(edgio_api.MockEdgioClient)
+
+	fixedTime := time.Date(2024, 10, 2, 10, 0, 0, 0, time.UTC)
+
+	uploadedCert := &dtos.TLSCertResponse{
+		ID:               "cert-456",
+		EnvironmentID:    "env-123",
+		Expiration:       fixedTime.Add(365 * 24 * time.Hour).Format(time.RFC3339),
+		Status:           "failed",
+		ActivationError:  "Certificate upload failed",
+		PrimaryCert:      "-----BEGIN CERTIFICATE-----1",
+		IntermediateCert: "-----BEGIN CERTIFICATE-----2",
+		Generated:        false,
+		Serial:           "0987654321",
+		CommonName:       "example.org",
+		AlternativeNames: []string{"www.example.org"},
+		CreatedAt:        fixedTime.Format(time.RFC3339),
+		UpdatedAt:        fixedTime.Format(time.RFC3339),
+	}
+
+	mockClient.On("UploadTlsCert", mock.AnythingOfType("dtos.UploadTlsCertRequest")).Return(uploadedCert, nil)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"edgio": providerserver.NewProtocol6WithError(edgio_provider.NewMockedProvider(mockClient)),
+		},
+		Steps: []resource.TestStep{
+			// Test uploading a TLS cert with a failed status
+			{
+				Config: `
+				provider "edgio" {
+					client_id     = "mock-client-id"
+					client_secret = "mock-client-secret"
+				}
+
+				resource "edgio_tls_cert" "uploaded" {
+					environment_id     = "env-123"
+					primary_cert       = "-----BEGIN CERTIFICATE-----1"
+					intermediate_cert  = "-----BEGIN CERTIFICATE-----2"
+					private_key        = "-----BEGIN PRIVATE KEY-----3"
+				}`,
+				ExpectError: regexp.MustCompile("Error: Certificate upload failed"),
 			},
 		},
 	})
